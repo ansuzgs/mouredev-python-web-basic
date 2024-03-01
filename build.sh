f875f4b7c5080ea368e189e81cae6b7c11eeacb5
ansuzgs <ansuz.gs@gmail.com>
2024-03-01 17:32:26 +0100

    Adding api url to frontend

--- build.sh
+++ build.sh
@@ -2,7 +2,7 @@ source .venv/bin/activate
 pip install --upgrade pip
 pip install -r requirements.txt
 reflex init
-reflex export --frontend-only
+API_URL=https://mouredev-python-web-basic-production.up.railway.app reflex export --frontend-only
 rm -rf public
 unzip frontend.zip -d public
 rm -f frontend.zip
